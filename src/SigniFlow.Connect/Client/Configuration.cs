3066c4fd64de61363e604f10dde6ef9868ef278a
Eugene Smit <eugenes@pbsa.co.za>
2021-07-12 11:41:15 +0200

    Version Update

--- src/SigniFlow.Connect/Client/Configuration.cs
+++ src/SigniFlow.Connect/Client/Configuration.cs
@@ -32,7 +32,7 @@ namespace SigniFlow.Connect.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -499,7 +499,7 @@ namespace SigniFlow.Connect.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0\n";
-            report += "    SDK Package Version: 1.0.0\n";
+            report += "    SDK Package Version: 1.0.1\n";
 
             return report;
         }
