5933da6953830cff3bc118444bfeff77b3a3fbf7
Morne Wilken <mornew@pbsa.co.za>
2022-05-05 16:22:13 +0200

    Updated version

--- src/SigniFlow.Connect/Client/Configuration.cs
+++ src/SigniFlow.Connect/Client/Configuration.cs
@@ -32,7 +32,7 @@ namespace SigniFlow.Connect.Client
         /// Version of the package.
         /// </summary>
         /// <value>Version of the package.</value>
-        public const string Version = "1.0.31";
+        public const string Version = "1.0.32";
 
         /// <summary>
         /// Identifier for ISO 8601 DateTime Format
@@ -499,7 +499,7 @@ namespace SigniFlow.Connect.Client
             report += "    OS: " + System.Environment.OSVersion + "\n";
             report += "    .NET Framework Version: " + System.Environment.Version  + "\n";
             report += "    Version of the API: 1.0\n";
-            report += "    SDK Package Version: 1.0.31\n";
+            report += "    SDK Package Version: 1.0.32\n";
 
             return report;
         }
