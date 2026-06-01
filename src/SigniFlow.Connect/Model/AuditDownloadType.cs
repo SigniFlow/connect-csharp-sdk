5461d74f4174ffe3f6405c729d07842cc1c45e57
Jeandre Lotter <jeandrel@signiflow.com>
2026-06-01 08:49:41 +0200

    Add AuditDownloadType enum and DownloadType to request model
    Introduced AuditDownloadType enum for audit document type selection. Updated GetAuditDocumentRequest to include DownloadType property and related logic. Added unit tests covering new property, constructors, validation, and serialization.

---
+++ src/SigniFlow.Connect/Model/AuditDownloadType.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System.Runtime.Serialization;
+
+namespace SigniFlow.Connect.Model
+{
+    /// <summary>
+    /// #### Sets download type to either   &#x60;0 &#x3D; Document 1 &#x3D; Combined&#x60;
+    /// </summary>
+    /// <value>#### Sets download type to either   &#x60;0 &#x3D; Document 1 &#x3D; Combined&#x60;</value>
+
+    [JsonConverter(typeof(StringEnumConverter))]
+
+    public enum AuditDownloadType
+    {
+        /// <summary>
+        /// Enum Document for value: 0
+        /// </summary>
+        [EnumMember(Value = "0")]
+        Document = 0,
+
+        /// <summary>
+        /// Enum Combined for value: 1
+        /// </summary>
+        [EnumMember(Value = "1")]
+        Combined = 1
+
+    }
+}
